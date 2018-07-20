8531dfcd8acf832d6281e162e2d44a01d04ea3a6
lukasbe95 <lukasbe@gazeta.pl>
2018-07-20 09:42:55 +0200

    Script for console edition done.

--- src/backuptabs.py
+++ src/backuptabs.py
@@ -25,10 +25,29 @@ def editFile(path):
         f.write(edited_message.decode("utf-8"))
         f.close()
         print ("File has been updated!")
+def listFile(path):
+    f = open(path, mode='r')
+    for line in f.readlines():
+        print(line,end='')
+    f.close()
+def clearFile(path):
+    f = open(path, mode='w')
+    f.close()
+    print("File is clear now.", sep=' ', end='\n', file=sys.stdout, flush=False)
 def optionParser():
-
+    parser = argparse.ArgumentParser(description='This is daemon\'s help. All options listed below!')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-e','--edit', help='Edit tab in vim',action='store_true')
+    group.add_argument('-l','--list', help='List whole tab (config file)',action='store_true')
+    group.add_argument('-c','--clear', help='Delete whole tab (clear config file)',action='store_true')
     # name of the file is still to upgrade :)
-    editFile("/home/lukas/dupa")
-
+    # editFile("/home/lukas/dupa")
+    args = parser.parse_args()
+    if args.edit:
+        editFile("/home/lukas/dupa")
+    elif args.list:
+        listFile("/home/lukas/dupa")
+    else:
+        clearFile("/home/lukas/dupa")
 if __name__ == '__main__':
     optionParser()
